b694c5dbf3f18f33f447399cc5d6c45e18e791ef
Hüseyin Can Çirkin <xyonogcal@gmail.com>
2024-04-06 23:19:39 +0300

    complete input component

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,12 +1,15 @@
 import Button from "../components/ui/Button";
 import iconPlus from "../assets/icons/icon-plus.svg";
+import Input from "../components/form/Input";
 
 export default function Root() {
   return (
     <div>
-      <Button type="dark" label="New Invoice">
-        {/* <img src={iconPlus} width={10} height={10}></img> */}
-      </Button>
+      {/* <Button type="dark" label="New Invoice">
+        <img src={iconPlus} width={10} height={10}></img>
+      </Button> */}
+
+      <Input />
     </div>
   );
 }
